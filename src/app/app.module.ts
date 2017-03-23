e72f0238ec48fd5c1d19a5c4b0a09e343b556983
rievosol <zakitechent@yahoo.com>
2017-03-23 19:38:20 +0800

    Reorganize codes and import additional services into the system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,47 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { RequestOptions } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 
+import { Api } from '../providers/api';
+import { DefaultRequestOptions } from '../providers/default-request-options';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+let pages = [
+  MyApp,
+  Page1,
+  Page2
+];
+
+export function declarations() {
+  return pages;
+}
+
+export function entryComponents() {
+  return pages;
+}
+
+export function providers() {
+  return [
+    Api,
+    StatusBar,
+    SplashScreen,
+    { provide: RequestOptions, useClass: DefaultRequestOptions },
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+  ]
+}
+
 @NgModule({
-  declarations: [
-    MyApp,
-    Page1,
-    Page2
-  ],
+  declarations: declarations(),
   imports: [
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    Page1,
-    Page2
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  entryComponents: entryComponents(),
+  providers: providers()
 })
 export class AppModule {}
